f9a9d4fe1cc8b8bb12755850fb26368b013434db
lionsin <111735154+BADyousawme@users.noreply.github.com>
2023-09-19 19:23:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x+"px"
+    blur.style.top = dets.y+"px"
+})
+
+
+
+
+
+gsap.to("#nav",{
+    backgroundColor:"black",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -12%",
+        scrub:1,
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor:"black",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2,
+    }
+})
